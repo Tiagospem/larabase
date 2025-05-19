7dd753ff753e82806f1b325aee7e09af6e4858f3
Tiago Padilha <tiagospem@gmail.com>
2025-05-19 08:37:52 -0300

    refactor: improve optional chaining for database configuration in project.ts

--- src/store/project.ts
+++ src/store/project.ts
@@ -7,7 +7,7 @@ export const useProjectStore = defineStore('project', () => {
 	const models = ref<Models>({ models: [], success: false });
 
 	const targetDatabase = computed(
-		() => selectedProject.value?.dbConfig.database || ''
+		() => selectedProject.value?.dbConfig?.database
 	);
 
 	const state = reactive({
